86a1b8055861e868b5c76fb623b5a29f357e9df2
JUAN-P-A-B-L-O <juanpablo25102001@gmail.com>
2022-09-01 13:48:49 -0300

    z

--- index.js
+++ index.js
@@ -1,39 +1,44 @@
-const twit = require(`twit`);
+const twit = require("twit");
 
-require(`dotenv`).config();
-
-const bot = new twit({
+require("dotenv").config();
+let id;
+const Bot = new twit({
     consumer_key: process.env.API_KEY,
     consumer_secret: process.env.API_SECRET_KEY,
     access_token: process.env.ACCESS_TOKEN,
     access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-    timeout_ms: 60*1000,
+    timeout_ms: 60 * 1000,
 });
 
-const botIit = () => {
-    const query = {
-        q: `#javascript`,
-        result_type: `recent`
+const botInit = () => {
+    let query = {
+        q: "#javascript",
+        result_type: "recent",
     };
-    bot.get(`search/tweets`, query, (error, data, response)=>{
-        if(error){
-            console.log(`Bot nao conseguiu realizar a busca dos ultimos tweets`);
-        }else{
-            const id= {
+
+
+    Bot.get("search/tweets", query, BotGotLatestTweet);
+    function BotGotLatestTweet(error, data, response) {
+
+        if (error) {
+            console.log("Bot nao conseguiu realizar a busca dos ultimos tweets");
+        } else {
+            id = {
                 id: data.statues[0].id_str,
             };
+        
+    }}
 
+    Bot.post("statuses/retweet/:id", id, BotRetweet);
+    function BotRetweet(error, response) {
+        if (error) {
+            console.log("nao deu certo o retwit", error);
+        } else {
+            console.log("Bot retweeto", id.id);
         }
-        bot.post(`statuses/retweet/:id`, id, (error, response)=>{
-            if(error){
-                console.log(`nao deu certo o retwit`, error);
-            }else{
-                console.log(`Bot retweeto`, id.id);
-            }
-
-        })
-    })
+    }
 }
 
-setInterval(botIit, 1*60*1000);
-botIit();
+
+setInterval(botInit, 1 * 60 * 1000);
+botInit();
