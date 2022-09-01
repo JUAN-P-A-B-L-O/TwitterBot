f54bcb5b87284625cc3d82e16bbb8d0b8fea72a8
JUAN-P-A-B-L-O <juanpablo25102001@gmail.com>
2022-09-01 13:17:36 -0300

    a

--- index.js
+++ index.js
@@ -10,4 +10,30 @@ const bot = new twit({
     timeout_ms: 60*1000,
 });
 
+const botIit = () => {
+    const query = {
+        q: `#javascript`,
+        result_type: `recent`
+    };
+    bot.get(`search/tweets`, query, (error, data, response)=>{
+        if(error){
+            console.log(`Bot nao conseguiu realizar a busca dos ultimos tweets`);
+        }else{
+            const id= {
+                id: data.statues[0].id_str,
+            };
 
+        }
+        bot.post(`statuses/retweet/:id`, id, (error, response)=>{
+            if(error){
+                console.log(`nao deu certo o retwit`, error);
+            }else{
+                console.log(`Bot retweeto`, id.id);
+            }
+
+        })
+    })
+}
+
+setInterval(botIit, 1*60*1000);
+botIit();
