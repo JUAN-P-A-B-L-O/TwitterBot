9e4f152bed7f18c8cc66e2be1c89c31eede634a7
JUAN-P-A-B-L-O <juanpablo25102001@gmail.com>
2022-09-01 13:08:30 -0300

    s

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const twit = require(`twit`);
+
+require(`dotenv`).config();
+
+const bot = new twit({
+    consumer_key: process.env.API_KEY,
+    consumer_secret: process.env.API_SECRET_KEY,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+    timeout_ms: 60*1000,
+});
+
+
